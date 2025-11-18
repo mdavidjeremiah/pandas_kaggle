9dc8752eb8c5dc7a38d69292085fc3c95db2766d
Muwanguzi David Jeremiah <122023849+mdavidjeremiah@users.noreply.github.com>
2025-11-18 12:54:47 +0000

    Added New Features

--- CRW.py
+++ CRW.py
@@ -1,7 +1,29 @@
 import pandas as pd
 
-data = pd.DataFrame({"Name": ["Muwanguzi", "David", "Jeremiah"],
+#Creating Data
+# DataFrame ==> A table containing an array of individual entries, each with a certain value
+# Each entry corresponds to a ROW(or RECORD) and a column.
+data1 = pd.DataFrame({"Yes":[53, 75], "No": [39, 82]})
+print(data1)
+print("="* 60)
+# DataFrame() constructor
+'''
+Used to generate DataFrame Objects.
+Syntax of declaring a new one is Dictionary whose KEYS are the COLUMN NAMES
+'''
+data2 = pd.DataFrame({"Name": ["Muwanguzi", "David", "Jeremiah"],
                 "Age": [20, 32, 15]},
                 index=['Person A', 'Person B', 'Person C'])
 
-print(data)
+print(data2)
+print("="* 60)
+
+#SERIES : A sequence of data values. If a DataFrame is a table, Series is a List
+# A Series is a single column of a DataFrame, u can assign row labels to the Series the same way as before, using an "index" parameter.
+
+serie1 = pd.Series([1, 2, 3, 4, 5])
+print(serie1)
+print("="*60)
+#If a Series does not have a column name, it only has one overall "name"
+serie2 = pd.Series([24,65,25,53], index=['2020 Sales', '2021 Sales', '2022 Sales', '2023 Sales'], name='Product A')
+print(serie2)
